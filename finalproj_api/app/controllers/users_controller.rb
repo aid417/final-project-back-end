87961893ecc32f122535754d721e4187c5fb45e7
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-12-16 18:09:08 -0500

    started email sending

--- finalproj_api/app/controllers/users_controller.rb
+++ finalproj_api/app/controllers/users_controller.rb
@@ -16,12 +16,19 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.welcome_email(@user).deliver_now
+ 
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
+  end
   end
 
   # PATCH/PUT /users/1
--- finalproj_api/app/mailers/application_mailer.rb
+++ finalproj_api/app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'ga.final.project.news@gmail.com'
   layout 'mailer'
 end
