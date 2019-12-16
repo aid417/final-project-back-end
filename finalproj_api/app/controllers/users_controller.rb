8a219f3efb4ba70888cc7e58085dc0f8838daf48
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-12-16 11:41:23 -0500

    fixed username problem

--- finalproj_api/app/controllers/users_controller.rb
+++ finalproj_api/app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :user_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
     end
 end
