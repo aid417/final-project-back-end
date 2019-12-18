e0f83066939c83d8ce1f36511229406d04061d4f
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-12-18 13:58:10 -0500

    added whenever gem to ruby file

--- finalproj_api/app/mailers/daily_mailer.rb
+++ finalproj_api/app/mailers/daily_mailer.rb
@@ -1,6 +1,6 @@
 class DailyMailer < ApplicationMailer
     def new_daily_email
         @user = params[:user]
-        mail(to: @user.email, subject: "You're a new user!")
+        mail(to: @user.email, subject: "Daily Digest")
     end
 end
